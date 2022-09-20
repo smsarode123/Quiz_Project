5e9902624c9ac4101d3b8432c1f11b8c3bafcab6
Sagar Sarode <sagarsarode1997@gmail.com>
2022-09-20 12:21:40 +0530

    In StudentDetalis: I provided getter and setter method

--- Quiz_Project/src/com/grouph/quizproject/StudentDetails.java
+++ Quiz_Project/src/com/grouph/quizproject/StudentDetails.java
@@ -1,5 +1,23 @@
 package com.grouph.quizproject;
 
 public class StudentDetails {
-
+	
+	private String studentRollNumber;
+	private String studentName;
+	
+//  Here we are providing public getter and setter method for private variable
+	
+	public String getRollNumber() {
+		return studentRollNumber;
+	}
+	public void setRollNumber(String studentRollNumber) {
+		this.studentRollNumber = studentRollNumber;
+	}
+	public String getName() {
+		return studentName;
+	}
+	public void setName(String studentName) {
+		this.studentName = studentName;
+	}	
 }
+
