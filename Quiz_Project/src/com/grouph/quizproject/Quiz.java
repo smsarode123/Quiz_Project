282a81245d0d136c02fe8fdbab1dbde349fc63d3
sonaljadhav07 <pkjadhav0020@gmail.com>
2022-09-20 14:47:46 +0530

    main method and start method added

--- Quiz_Project/src/com/grouph/quizproject/Quiz.java
+++ Quiz_Project/src/com/grouph/quizproject/Quiz.java
@@ -1,5 +1,37 @@
 package com.grouph.quizproject;
 
-public class Quiz {
+import java.sql.SQLException;
+import java.util.Scanner;
 
+public class Quiz {
+	
+	Scanner sc=new Scanner(System.in);
+	/*
+	 * Here we are creating one method which is use to interact with user to
+	 * Login and Registration
+	 */
+	public void quizStart() {
+		LoginAndRegistration studentlar=new LoginAndRegistration();
+		System.out.println("For Login type 'LOGIN' and if you are new then please type 'REGISTER'");
+		String checkNew=sc.next().toUpperCase();
+		if(checkNew.equals("LOGIN")) {
+			// Here we are calling studentLogin method
+			studentlar.studentLogin();
+		}else if(checkNew.equals("REGISTER")){
+			// Here we are calling studentRegistration method
+			studentlar.studentRegistration();
+		}else {
+			System.out.println("Please submit your answer correctly..");
+			// Here we are calling quizStart method again to continue the loop
+			quizStart();
+		}
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Quiz quiz=new Quiz();
+		// Here we are calling quizStart method
+		quiz.quizStart();
+		
+	}
 }
