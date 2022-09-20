cd4a961de37598dda46ddeffa512c187f02e76cd
ramthorve <ramthorve12@gmail.com>
2022-09-20 12:03:03 +0530

    connection method added

--- Quiz_Project/src/com/grouph/quizproject/GetConnection.java
+++ Quiz_Project/src/com/grouph/quizproject/GetConnection.java
@@ -1,5 +1,19 @@
 package com.grouph.quizproject;
 
-public class GetConnection {
+import java.sql.*;
 
+public class GetConnection {
+	
+	Connection con=null;
+	//creating connection
+	public Connection getConnectionDetails() throws SQLException {
+		System.out.println("GetConnection....");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3308/example", "root", "Sagar@142143");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
